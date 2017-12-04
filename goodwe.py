664ea40337b9a777f612f3d31a4d12717ccb3628
Pelle van der Heide <P.S.vanderHeide@gmail.com>
2017-12-04 17:19:32 +0100

    Add real time data, add language
    Add scraping of real-time data
    As the units are localized and default to Chinese, function to set
    language from config file
    Supported languages are: "en-US, zh-CN, ko-KR, it-IT, cs-CZ"
    
    Add dependency on lxml to requirements for parsing html

--- goodwe.py
+++ goodwe.py
@@ -3,6 +3,8 @@ import logging
 import configparser
 import os
 import errno
+from lxml import html
+
 
 def make_sure_path_exists(path):
     try:
@@ -10,6 +12,7 @@ def make_sure_path_exists(path):
     except OSError as exception:
         if exception.errno != errno.EEXIST:
             raise
+
 
 class ScraperSession(requests.Session):
     def __init__(self, configfile='goodwe.cfg'):
@@ -22,9 +25,11 @@ class ScraperSession(requests.Session):
         self.inverterSN = config['inverterSN']
         self.username = config['username']
         self.password = config['password']
+        self.language = config['language']
 
         self.home()
         self.login()
+        self.set_language()
 
     def home(self):
         logging.info("Opening main site")
@@ -37,6 +42,34 @@ class ScraperSession(requests.Session):
                    "password": self.password}
         login_url = "http://www.goodwe-power.com/User/Login"
         result = self.post(login_url, data=payload)
+
+    def set_language(self):
+        """
+        Set language of website
+        Supported are: "en-US, zh-CN, ko-KR, it-IT, cs-CZ"
+        """
+        logging.info(f"Setting language: {self.language}")
+        payload = {"language": self.language}
+        url = "http://www.goodwe-power.com/User/ChangeLanguage"
+        result = self.post(url, data=payload)
+
+    def get_real_time_data(self):
+        logging.info(f"Requesting real-time-data")
+        url = "http://www.goodwe-power.com/PowerStationPlatform/PowerStationReport/PowerCompare"
+        url += f"?ID={self.stationId}"
+        result = self.get(url)
+        tree = html.fromstring(result.content)
+        elements = tree.xpath('//ul[@class="unit clearfix"]')[0]
+        tags = elements.xpath('//div[@class="num"]')
+        result = {}
+        for tag in tags:
+            data = tag.xpath('p')
+            name = data[0].values()[0]
+            value = data[0].text.strip()
+            data = tag.xpath('i')
+            unit = data[0].text.strip()
+            result[name] = {"value": value, "unit": unit}
+        print(result)
 
     def request_export(self, date):
         logging.info(f"Requesting export for {date}")
@@ -69,6 +102,7 @@ if __name__ == "__main__":
     logging.basicConfig(format='%(asctime)s %(name)s:%(levelname)s:%(message)s', level=logging.INFO)
     dates = [f"2016-{month:02}-01" for month in range(1, 13)]
     with ScraperSession() as s:
+        s.get_real_time_data()
         for date in dates:
             export = s.request_export(date)
             s.download_export(export)
